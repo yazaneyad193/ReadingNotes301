9fd8cdac35e869e451152576683494060745a2d2
yazan eyad alsharee <yazaneyad193@gmail.com>
2021-02-23 18:30:22 +0200

    Create ReadLab09.m

---
+++ ReadLab09.m
@@ -0,0 +1,31 @@
+# Code Refactor 
+
+## Description 
+This project's purpose was to refactor a webpage so that the links are function, CSS selectors/properties are consolidated and sematic and to optimize the code for search engines. 
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation 
+No installation needed. 
+
+## Usage 
+This is not a function application. It is for test purposes.<br>
+Please view deployed Git Hub [URL](https://nicolewallace09.github.io/challenge-code-refactor/)
+<img src="./assets/images/Website-1.png">
+<img src="./assets/images/Website-2.png">
+
+
+## License 
+This project is license under MIT
+
+## Contributing 
+Contributors should read the installation section. 
+
+## Tests
+There are no tests for this application. 
